3ae386065ad63ab4503ac011742150fbac77f982
andriy-kolokolov <koloandre22@gmail..com>
2023-08-18 16:57:14 +0200

    kyu 7 katas solved

--- java/KataSimple.java
+++ java/KataSimple.java
@@ -1,8 +1,13 @@
+import java.util.Arrays;
 import java.util.stream.Collectors;
 
-public class Kata {
+public class KataSimple {
     public static void main(String[] args) {
-        System.out.println(squareDigits(6969));
+        Arrays.stream(map(new int[]{1, 2, 3, 4, 5})).forEach(x -> System.out.println(x));
+    }
+
+    public static int[] map(int[] arr) {
+        return Arrays.stream(arr).map(x -> x * x).toArray();
     }
 
     public static int squareDigits(int n) {
@@ -18,7 +23,7 @@ public class Kata {
 
     public static String areYouPlayingBanjo(String name) {
         return name.toLowerCase().startsWith("r") ?
-                name + " plays banjo"  :
+                name + " plays banjo" :
                 name + " does not play banjo";
     }
 }
