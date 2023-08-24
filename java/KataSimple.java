81f19344d6fd2e37df72878fd037d280df01b59f
andriy-kolokolov <koloandre22@gmail.com>
2023-08-24 10:33:08 +0200

    kyu 7 katas solved

--- java/KataSimple.java
+++ java/KataSimple.java
@@ -1,9 +1,22 @@
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.stream.Collectors;
 
 public class KataSimple {
     public static void main(String[] args) {
-        Arrays.stream(map(new int[]{1, 2, 3, 4, 5})).forEach(x -> System.out.println(x));
+        System.out.println(sortDesc(1423));
+    }
+
+    public static int sortDesc(final int num) {
+        return Integer.parseInt(
+                String.valueOf(num)
+                        .chars()
+                        .map(Character::getNumericValue)
+                        .boxed()  // Convert IntStream to Stream<Integer>
+                        .sorted(Comparator.reverseOrder())  // Sort in descending order
+                        .map(String::valueOf)
+                        .collect(Collectors.joining())
+        );
     }
 
     public static int[] map(int[] arr) {
