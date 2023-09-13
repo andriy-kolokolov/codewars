eeebca8aec85b3da5c89527f7bb937e335f06b84
andriy-kolokolov <koloandre22@gmail.com>
2023-09-13 15:10:21 +0200

    solved

--- java/Kyu8.java
+++ java/Kyu8.java
@@ -1,6 +1,18 @@
+import java.util.stream.Collectors;
+
 public class Kyu8 {
     public static void main(String[] args) {
-        System.out.println(isEven(0));
+        System.out.println(doubleChar2("double char"));
+    }
+
+    static String doubleChar2(String s) {
+        return s.replaceAll(".", "$0$0");
+    }
+
+    public static String doubleChar(String s){
+        return s.chars()
+                .mapToObj(c -> String.valueOf((char) c) + (char) c)
+                .collect(Collectors.joining());
     }
 
     public static boolean isEven(double n) {
